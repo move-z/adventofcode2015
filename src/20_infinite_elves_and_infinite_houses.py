81ff0dbda846c6a0cd2b9abda64c9b04c24062bb
Rodolfo Boer <rodolfo.boer@gmail.com>
2018-03-17 12:10:25 +0100

    day 20 part 2

--- src/20_infinite_elves_and_infinite_houses.py
+++ src/20_infinite_elves_and_infinite_houses.py
@@ -2,27 +2,44 @@
 
 
 def first(limit):
-    sz = limit // 10
-    houses = [0 for i in range(sz)]
+    def calc(arr_sz):
+        houses = [0 for i in range(arr_sz)]
 
-    for i in range(1, sz):
-        for j in range(i, sz, i):
-            houses[j] += i
+        for i in range(1, arr_sz):
+            for j in range(i, arr_sz, i):
+                houses[j] += i
 
-    for i in range(len(houses)):
-        print("%d %d" % (i, houses[i]))
-        if houses[i] > limit // 10:
-            return i
+        for i in range(len(houses)):
+            if houses[i] > limit // 10:
+                return i
+
+    for arr_sz in reversed(range(1, 100, 10)):
+        x = calc(limit // arr_sz)
+        if x is not None:
+            return x
 
 
 def second(limit):
-    pass
+    def calc(arr_sz):
+        houses = [0 for i in range(arr_sz)]
+
+        for i in range(1, arr_sz):
+            for j in range(i, min(arr_sz, i * 51), i):
+                houses[j] += i * 11
+
+        for i in range(len(houses)):
+            if houses[i] > limit:
+                return i
+
+    for arr_sz in reversed(range(1, 100, 10)):
+        x = calc(limit // arr_sz)
+        if x is not None:
+            return x
 
 
 if __name__ == '__main__':
     res = first(36_000_000)
     print(">>> %s" % res)
 
-    # res = second(data)
-    # print(">>> %s" % res)
-
+    res = second(36_000_000)
+    print(">>> %s" % res)
